7ab18b422a7f7c9c1a2a2e8d5bd0bc8f66138c20
Suan Choi <siasiarhdwn@gmail.com>
2023-03-02 20:47:46 +0800

    feat: add range slider
    - add custom range slider
    - add thumb style and background color change function

--- src/components/UI/Slider.tsx
+++ src/components/UI/Slider.tsx
@@ -1,18 +1,30 @@
 import { InputHTMLAttributes, useState } from 'react';
 import './Slider.scss'
 
-interface SliderProps extends InputHTMLAttributes<HTMLInputElement>{
+interface SliderProps extends InputHTMLAttributes<HTMLInputElement> {
   min: number,
   max: number,
   value: number,
   type: string,
-  name: string
-  onChange: (e:React.ChangeEvent<HTMLInputElement>) => void
+  name: string,
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const Slider = (props: SliderProps) => {
+const Slider = (props: SliderProps): JSX.Element => {
+  const getBackgroundSize = () => {
+    return { backgroundSize: `${((props.value-props.min) * 100) / (props.max-props.min)}% 100%` }
+  }
   return (
-    <input name={props.name} type={props.type} step='1' min={props.min} max={props.max} value={props.value} className='slider' onChange={props.onChange} />
+    <input 
+      name={props.name} 
+      type={props.type} 
+      step='1' 
+      min={props.min} 
+      max={props.max} 
+      value={props.value} 
+      className='slider' 
+      style={getBackgroundSize()}
+      onChange={props.onChange}/>
   )
 }
 
