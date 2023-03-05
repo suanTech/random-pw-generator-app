61cf5bfd44e1b55c369869ab6150b45690451f50
Suan Choi <siasiarhdwn@gmail.com>
2023-03-05 11:13:39 +0800

    feat: add alert to samsung browser user for display

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
 import './app.scss';
 import { Form } from './components/form/Form';
 import { AppcontextProvider } from './context/context';
 
 function App() {
+  useEffect(() => {
+    if (navigator.userAgent.match(/samsung/i)) {
+      alert(`You are using a defective browser (Samsung Internet) that might not be configured to display this website properly.
+      You should consider using a proper standards-compliantbrowser instead. \n\n
+      We recommend using Firefox, Microsoft Edge, or Google Chrome.`);
+    }}, [navigator])
 
   return (
     <AppcontextProvider>
